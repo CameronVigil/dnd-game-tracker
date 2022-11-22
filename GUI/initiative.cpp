e6167eccd355b77220c34130a3b6cfce31384365
JJGroce95 <108232216+JJGroce95@users.noreply.github.com>
2022-11-22 17:36:34 -0600

    Update initiative.cpp

--- GUI/initiative.cpp
+++ GUI/initiative.cpp
@@ -1,4 +1,16 @@
 #include "initiative.h"
+#include <algorithm>
+#include <vector>
+
+void Init::TEMPnames()
+{
+   p[0].name = "Sam";
+   p[1].name = "Tom";
+   p[2].name = "Sally";
+   p[3].name = "Bill";
+   p[4].name = "Robert";
+
+}
 
 int Init::getInit(string name)
 {
@@ -9,26 +21,47 @@ int Init::getInit(string name)
 //Assigns Initiative to each player
 void Init::AssignInit()
 {
+    //each player gets initiative value
     for(int i = 0; i < num_p; i++)
     {
         p[i].initiative = getInit(p[i].name);
     }
+    //order is sorted in descending order
+    SortOrder();
 }
 
+//Sort in descending order
 void Init::SortOrder()
 {
+
+    //assign Name to order of combat (unsorted)
     for(int i = 0; i < num_p; i++)
     {
         combatOrder[i] = p[i].name;
     }
 
-    for(int i = num_p; i >= 0; i--)
+    int tempArr[num_p];
+    //copy p[n].initiative to a temporary array
+    for(int i = 0; i<num_p; i++)
     {
-        for(int j = num_p; j > num_p - i; j--)
+        tempArr[i] = p[i].initiative;
+    }
+
+    //Sort tempArr
+    for(int i = 0; i<num_p; i++)
+    {
+        for(int j = i+1; j < num_p; j++)
         {
-            if(p[j].initiative > p[j-1].initiative)
+            if( tempArr[i] < tempArr[j])
             {
-                swap(combatOrder[j], combatOrder[j-1]);
+                //sort temporary array
+                int temp = tempArr[i];
+                tempArr[i] = tempArr[j];
+                tempArr[j] = temp;
+                //reflect this in combat order
+                string temp1 = combatOrder[i];
+                combatOrder[i] = combatOrder[j];
+                combatOrder[j] = temp1;
             }
         }
     }
